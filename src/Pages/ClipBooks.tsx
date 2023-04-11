d5e2db04b921c43edb27bd0d65bcc6050ed5ff3d
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-04-12 00:54:46 +0530

    clipbook created

--- src/Pages/ClipBooks.tsx
+++ src/Pages/ClipBooks.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import "./ClipBook.css";
 
 function ClipBooks() {
-  return <div>ClipBooks</div>;
+  const clipBooks = [
+    {
+      id: 1,
+      lable: "Instagram",
+    },
+    {
+      id: 2,
+      lable: "Instagram",
+    },
+    {
+      id: 3,
+      lable: "Instagram",
+    },
+    {
+      id: 4,
+      lable: "Instagram",
+    },
+    {
+      id: 5,
+      lable: "Instagram",
+    },
+    {
+      id: 3,
+      lable: "Instagram",
+    },
+    {
+      id: 4,
+      lable: "Instagram",
+    },
+    {
+      id: 5,
+      lable: "Instagram",
+    },
+  ];
+
+  return (
+    <div className="book_box">
+      <form action="" className="book_form">
+        <label className="clip_lable">Enter clipbook name</label>
+        <input className="book_input" type="text" />
+      </form>
+      <div className="book_container">
+        {clipBooks.map(({ id, lable }) => (
+          <div key={id} className="clip_book">
+            <h1>{lable}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ClipBooks;
